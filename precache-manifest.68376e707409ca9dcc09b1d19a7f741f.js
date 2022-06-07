4bd72f2400e8179b6dbc8a1eb822e6b12f9fc122
Dec <dec@Oluwaseun.local>
2022-06-07 18:25:43 +0100

    Updates

---
+++ precache-manifest.68376e707409ca9dcc09b1d19a7f741f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8687e180fbf40f8a865",
+    "url": "/code-snippets/static/css/main.7cd5c7d8.chunk.css"
+  },
+  {
+    "revision": "c8687e180fbf40f8a865",
+    "url": "/code-snippets/static/js/main.c8687e18.chunk.js"
+  },
+  {
+    "revision": "5a829b02006aefd4ea1d",
+    "url": "/code-snippets/static/css/1.f181f322.chunk.css"
+  },
+  {
+    "revision": "5a829b02006aefd4ea1d",
+    "url": "/code-snippets/static/js/1.5a829b02.chunk.js"
+  },
+  {
+    "revision": "b0e155417dc187d1f248",
+    "url": "/code-snippets/static/js/runtime~main.b0e15541.js"
+  },
+  {
+    "revision": "18c05d2462e5467489f69d2ca6cd0ec6",
+    "url": "/code-snippets/index.html"
+  }
+];
